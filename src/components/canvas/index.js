194930ff2cc7f60ec37f3dbc9e6c87a383011c5a
DankiDon <e0415441@u.nus.edu>
2024-01-19 20:39:07 +0800

    Delete src directory

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import PlanesCanvas from "./Planes";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, PlanesCanvas, StarsCanvas };
